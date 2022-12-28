3245450db3735a74cf8b74f6c39b613c8a9aea6d
Pranieri <psranieric@gmail.com>
2022-12-27 21:56:59 -0300

    new commit

--- js/script.js
+++ js/script.js
@@ -1,43 +1,85 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-const colorForm = document.getElementById('color-form');
-const borrowColorInput = document.getElementById('borrow-color');
+let calendar = document.querySelector('.calendar')
 
-let currentColor = '#ff0000';
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-colorForm.addEventListener('change', (event) => {
-  currentColor = event.target.value;
-});
+isLeapYear = (year) => {
+    return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 ===0)
+}
 
-let painting = false;
-let borrowing = false;
+getFebDays = (year) => {
+    return isLeapYear(year) ? 29 : 28
+}
 
-canvas.addEventListener('mousedown', (event) => {
-  painting = true;
-  if (borrowing) {
-    const pixelData = ctx.getImageData(event.offsetX, event.offsetY, 1, 1).data;
-    currentColor = `rgb(${pixelData[0]}, ${pixelData[1]}, ${pixelData[2]})`;
-    borrowColorInput.checked = false;
-    borrowing = false;
-  }
-  drawPixel(event.offsetX, event.offsetY, currentColor);
-});
+generateCalendar = (month, year) => {
 
-canvas.addEventListener('mousemove', (event) => {
-  if (painting) {
-    drawPixel(event.offsetX, event.offsetY, currentColor);
-  }
-});
+    let calendar_days = calendar.querySelector('.calendar-days')
+    let calendar_header_year = calendar.querySelector('#year')
 
-canvas.addEventListener('mouseup', () => {
-  painting = false;
-});
+    let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-borrowColorInput.addEventListener('change', () => {
-  borrowing = borrowColorInput.checked;
-});
+    calendar_days.innerHTML = ''
 
-function drawPixel(x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, 1, 1);
+    let currDate = new Date()
+    if (month > 11 || month < 0) month = currDate.getMonth()
+    if (!year) year = currDate.getFullYear()
+
+    let curr_month = `${month_names[month]}`
+    month_picker.innerHTML = curr_month
+    calendar_header_year.innerHTML = year
+
+    // get first day of month
+    
+    let first_day = new Date(year, month, 1)
+
+    for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
+        let day = document.createElement('div')
+        if (i >= first_day.getDay()) {
+            day.classList.add('calendar-day-hover')
+            day.innerHTML = i - first_day.getDay() + 1
+            day.innerHTML += `<span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>`
+            if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
+                day.classList.add('curr-date')
+            }
+        }
+        calendar_days.appendChild(day)
+    }
+}
+
+let month_list = calendar.querySelector('.month-list')
+
+month_names.forEach((e, index) => {
+    let month = document.createElement('div')
+    month.innerHTML = `<div data-month="${index}">${e}</div>`
+    month.querySelector('div').onclick = () => {
+        month_list.classList.remove('show')
+        curr_month.value = index
+        generateCalendar(index, curr_year.value)
+    }
+    month_list.appendChild(month)
+})
+
+let month_picker = calendar.querySelector('#month-picker')
+
+month_picker.onclick = () => {
+    month_list.classList.add('show')
+}
+
+let currDate = new Date()
+
+let curr_month = {value: currDate.getMonth()}
+let curr_year = {value: currDate.getFullYear()}
+
+generateCalendar(curr_month.value, curr_year.value)
+
+document.querySelector('#prev-year').onclick = () => {
+    --curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
+}
+
+document.querySelector('#next-year').onclick = () => {
+    ++curr_year.value
+    generateCalendar(curr_month.value, curr_year.value)
 }
